1f9429bf78a21c23958d07fc11837a54d9d8dba5
Timo Sneijder <89972625+defecon@users.noreply.github.com>
2021-09-14 13:37:50 +0200

    Add files via upload

---
+++ feestlunch.py.py
@@ -0,0 +1,11 @@
+aantalCroissantjes=18
+prijsCroissantjes=0.39
+aantalStokbroden=2
+prijsStokborden=2.78
+aantalkortingsbonnen=3
+totaalPrijs=aantalCroissantjes*prijsCroissantjes + aantalStokbroden * prijsStokborden - aantalkortingsbonnen
+
+
+print(" de feestlunch kost je bij de bakker " + str(totaalPrijs) + " voor de " + str(aantalCroissantjes)
+      + " croissantjes en de " + str(aantalStokbroden) + " stokbroden "
+      + " als de" + str(aantalkortingsbonnen) + "kortingsbonnen nog geldig zijn!")
---
+++ speelhaldag.py
@@ -0,0 +1,11 @@
+aantalPersonen=4
+aantalMinuten=45
+toegangsTicket=7.45
+gameseatTicket=3.33
+totaalPrijs=aantalPersonen*toegangsTicket + gameseatTicket
+
+
+print(" Dit geweldige dagje-uit met " + str(aantalPersonen) + " mensen in de speelhal met " + str(aantalMinuten)
++ " minuten VR kost je maar " + str(totaalPrijs) + " euro ")
+
+
